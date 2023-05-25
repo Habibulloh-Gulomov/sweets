517350332fc80307a97a2d865b69f0a51ca6a21c
habib-gulomov <gulomovhabibulloh@gmail.com>
2023-05-25 16:45:39 +0500

    add all

---
+++ sweets task2/sweets task2/Lasagna.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweets_task2
+{
+    internal class Lasagna
+    {
+        public int ExpectedMinutesInOven()
+        {
+            return 40;
+        }
+
+        public int RemainingMinutesInOven(int minutes)
+        {
+            return 40 - minutes;
+        }
+
+        public int PreparationTimeInMinutes(int layers)
+        {
+           int minutes = 0;
+           for(int i = 0; i < layers; i++)
+            {
+                minutes += 2;
+            }
+           return minutes;
+        }
+
+        public  int ElapsedTimeInMinutes(int layers, int minutes)
+        {
+            var layer = layers * 2;
+            var left = 40 - minutes;
+            var result = left+layer;
+            return result;
+        }
+    }
+}
---
+++ sweets task2/sweets task2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using sweets_task2;
+var lasagna = new Lasagna();
+var result = lasagna.ElapsedTimeInMinutes(3,20);
+Console.WriteLine(result);
